32ef79d88b07e6976ff25f8d33c7b6ee407db06f
Augusto <jesus_augga_99@icloud.com>
2022-05-10 18:34:45 -0500

    now reserved rockets are shown in profile

--- src/components/ProfileRockets.js
+++ src/components/ProfileRockets.js
@@ -1,12 +1,20 @@
-const ProfileRockets = () => (
-  <div className="profileRockets">
-    <h2 className="profileH2">My Rockets</h2>
-    <ul>
-      <li className="profileLi">Test</li>
-      <li className="profileLi">Test</li>
-      <li className="profileLi">Test</li>
-    </ul>
-  </div>
-);
+import { useSelector } from 'react-redux';
+
+const ProfileRockets = () => {
+  const rockets = useSelector((state) => state.rockets);
+
+  return (
+    <div className="profileRockets">
+      <h2 className="profileH2">My Rockets</h2>
+      <ul>
+        {rockets.filter((rocket) => rocket.isReserved).map((rocket) => (
+          <li key={rocket.id} className="profileLi">
+            {rocket.name}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 export default ProfileRockets;

02c610a1a13c3d38ecea913c3950e163dce9ccbd
Augusto <jesus_augga_99@icloud.com>
2022-05-10 18:19:08 -0500

    setup profile page

---
+++ src/components/ProfileMissions.js
@@ -0,0 +1,11 @@
+const ProfileMissions = () => (
+  <div className="profileMissions">
+    <h2>My Missions</h2>
+    <ul>
+      <li>Test</li>
+      <li>Test</li>
+    </ul>
+  </div>
+);
+
+export default ProfileMissions;
---
+++ src/components/ProfileRockets.js
@@ -0,0 +1,12 @@
+const ProfileRockets = () => (
+  <div className="profileRockets">
+    <h2>My Rockets</h2>
+    <ul>
+      <li>Test</li>
+      <li>Test</li>
+      <li>Test</li>
+    </ul>
+  </div>
+);
+
+export default ProfileRockets;

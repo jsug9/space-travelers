36d46e3c5a6108afbeee0c6a7e07d4a350fb2c68
Augusto <jesus_augga_99@icloud.com>
2022-05-10 16:35:30 -0500

    fetch rockets

--- src/components/RocketsList.js
+++ src/components/RocketsList.js
@@ -1,32 +1,17 @@
+import { useSelector, useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import { getRockets } from '../redux/rockets/rockets';
 import RocketItem from './RocketItem';
 
 const RocketsList = () => {
-  const testItems = [
-    {
-      id: '1',
-      name: 'Name',
-      description:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-      image: 'image',
-    },
-    {
-      id: '2',
-      name: 'Name',
-      description:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-      image: 'image',
-    },
-    {
-      id: '3',
-      name: 'Name',
-      description:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-      image: 'image',
-    },
-  ];
+  const dispatch = useDispatch();
+  const rockets = useSelector((state) => state.rockets);
+  useEffect(() => {
+    dispatch(getRockets());
+  }, []);
 
   return (
-    testItems.map((rocket) => (
+    rockets.map((rocket) => (
       <RocketItem key={rocket.id} rocket={rocket} />
     ))
   );

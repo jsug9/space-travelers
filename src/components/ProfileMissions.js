b434981451f18e473006601163251dd0efb60d8f
John-Arboleda <johnjack0126@gmail.com>
2022-05-11 17:56:44 -0500

    Add Joined Missions to My Profile

--- src/components/ProfileMissions.js
+++ src/components/ProfileMissions.js
@@ -1,11 +1,32 @@
-const ProfileMissions = () => (
-  <div className="profileSubContainer">
-    <h2 className="profileH2">My Missions</h2>
-    <ul>
-      <li className="profileLi">Test</li>
-      <li className="profileLi">Test</li>
-    </ul>
-  </div>
-);
+import { useSelector } from 'react-redux';
+
+const ProfileMissions = () => {
+  const missions = useSelector((state) => state.missions);
+  const filteredMissions = missions.filter((mission) => mission.isJoined);
+
+  const renderList = () => {
+    if (filteredMissions.length) {
+      return (
+        filteredMissions.map((mission) => (
+          <li key={mission.mission_id} className="profileLi">
+            <p className="profileLiLeft">
+              {mission.mission_name}
+            </p>
+          </li>
+        ))
+      );
+    }
+    return <li className="emptyLi">No missions joined</li>;
+  };
+
+  return (
+    <div className="profileSubContainer">
+      <h2 className="profileH2">My Missions</h2>
+      <ul>
+        {renderList()}
+      </ul>
+    </div>
+  );
+};
 
 export default ProfileMissions;

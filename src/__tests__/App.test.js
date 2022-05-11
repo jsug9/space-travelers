38480101c7520e324f4cd8ecf31ba1504ab92f10
Augusto <jesus_augga_99@icloud.com>
2022-05-11 08:53:49 -0500

    move app tests to tests folder

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import { Provider } from 'react-redux';
-import store from './redux/configureStore';
-import App from './App';
+import store from '../redux/configureStore';
+import App from '../App';
 
 test('renders learn react link', () => {
   render(
